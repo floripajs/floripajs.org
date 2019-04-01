1a510d8cca0568134fe43621eb4e8c779695af83
Diogo Moretti <diogo.pearljam@gmail.com>
2019-04-01 15:21:48 -0300

    new website

--- gulpfile.js
+++ gulpfile.js
@@ -1,130 +1,101 @@
-// Load Gulp and your plugins
-var gulp        = require('gulp'),
-    connect     = require('gulp-connect'),
-    stylus      = require('gulp-stylus'),
-    plumber     = require('gulp-plumber'),
-    watcher     = require('gulp-watch'),
-    rename      = require('gulp-rename'),
-    hbs         = require('gulp-compile-handlebars'),
-    yaml        = require('js-yaml'),
-    fs          = require('graceful-fs'),
-    del         = require('del'),
-    ghPages     = require('gulp-gh-pages'),
-    runSequence = require('run-sequence');
+const gulp = require('gulp')
+const connect = require('gulp-connect')
+const stylus = require('gulp-stylus')
+const plumber = require('gulp-plumber')
+const watcher = require('gulp-watch')
+const rename = require('gulp-rename')
+const hbs = require('gulp-compile-handlebars')
+const yaml = require('js-yaml')
+const fs = require('graceful-fs')
+const del = require('del')
+const ghPages = require('gulp-gh-pages')
+const runSequence = require('run-sequence')
+const nib = require('nib')
+const rupture = require('rupture')
+const koutoSwiss = require('kouto-swiss')
 
-/*
-  TASK: Connect
-  DESC: Create a server with livereload
-*/
-gulp.task('connect', function() {
-    connect.server({
-        root: 'dist',
-        livereload: true,
-        port: 5000
-    });
-});
+gulp.task('connect', () => {
+  connect.server({
+    root: './dist',
+    port: 7777,
+    livereload: true,
+    host: '0.0.0.0'
+  })
+})
 
-/*
-  TASK: Handlebars
-  DESC: Compile handlebars files (.hbs) in html files
-*/
-gulp.task('handlebars', function () {
-    var config = yaml.safeLoad(fs.readFileSync('./_config.yml', 'utf-8'));
+gulp.task('handlebars', () => {
+  const config = yaml.safeLoad(fs.readFileSync('./_config.yml', 'utf-8'))
     
-    gulp.src('./src/handlebars/*.hbs')
-        .pipe(hbs({
-            config: config
-        }))
-        .pipe(rename('index.html'))
-        .pipe(gulp.dest('./dist'))
-        .pipe(connect.reload());
-});
+  return gulp.src('./src/handlebars/*.hbs')
+    .pipe(hbs({
+      config
+    }))
+    .pipe(rename('index.html'))
+    .pipe(gulp.dest('./dist'))
+    .pipe(connect.reload())
+})
 
-/*
-  TASK: Stylus
-  DESC: Compile stylus files (.styl) in css files
-*/
-gulp.task('stylus', function () {
-    gulp.src('./src/stylus/*.styl')
-        .pipe(plumber())
-        .pipe(stylus({
-            use: ['nib'], 
-            set: ['compress']
-        }))
-        .pipe(gulp.dest('./dist/assets/css'))
-        .pipe(connect.reload());
-});
+gulp.task('stylus', () => {
+  return gulp.src('./src/stylus/*.styl')
+    .pipe(plumber())
+    .pipe(stylus({
+      compress: false,
+      use: [nib(), rupture(), koutoSwiss()],
+      import: ['nib', 'kouto-swiss']
+    }))
+    .pipe(gulp.dest('./dist/assets/css'))
+    .pipe(connect.reload())
+})
 
-/*
-  TASK: Watch
-  DESC: Copy and Paste modify files from src to dist
-*/
 gulp.task('watch:assets', function() {  
-    gulp.src('./src/assets/**/*')
-        .pipe(watcher('./src/assets/**/*'))
-        .pipe(gulp.dest('./dist/assets/'));
-});
+  gulp.src('./src/assets/**/*')
+    .pipe(watcher('./src/assets/**/*'))
+    .pipe(gulp.dest('./dist/assets/'))
+})
 
-/*
-  TASK: Copy
-  DESC: Copy assets files from src to dist
-*/
-gulp.task('copy:assets', function() {
-    return gulp.src('./src/assets/**/*')
-        .pipe(gulp.dest('./dist/assets'));
-});
+gulp.task('copy:assets', () => {
+  return gulp.src('./src/assets/**/*')
+    .pipe(gulp.dest('./dist/assets'))
+})
 
-/*
-  TASK: Clean
-  DESC: Delete dist folder
-*/
-gulp.task('clean:dist', function () {
-  return del('./dist');
-});
+gulp.task('clean:dist', () => {
+  return del('./dist')
+})
 
-/*
-  TASK: CNAME
-  DESC: Create a CNAME file for gh-pages
-*/
-gulp.task('cname', function() {
-    return fs.writeFileSync('./dist/CNAME', 'floripajs.org');
-});
+gulp.task('cname', () => {
+  return fs.writeFileSync('./dist/CNAME', 'floripajs.org')
+})
 
-/*
-  TASK: gh-pages
-  DESC: Deploy dist folder to 'gh-pages' remote branch
-*/
-gulp.task('deploy', function() {
-    return gulp.src('./dist/**/*')
-        .pipe(ghPages({
-            force: true
-        }));
-});
+gulp.task('deploy', () => {
+  return gulp.src('./dist/**/*')
+    .pipe(ghPages({
+      force: true
+    }))
+})
 
-/*
-  TASK: Watch
-  DESC: Verify css and hbs files
-*/
-gulp.task('watch', function () {
-    gulp.watch('./src/stylus/**/*.styl', ['stylus']);
-    gulp.watch(['./src/handlebars/*.hbs', './*.yml'], ['handlebars']);
-});
+gulp.task('watch', () => {
+  gulp.watch('./src/stylus/**/*.styl', ['stylus'])
+  gulp.watch(['./src/handlebars/*.hbs', './*.yml'], ['handlebars'])
+})
 
-// Set tasks
-gulp.task('build', function() {
-    runSequence('clean:dist',
-                'copy:assets',
-                'stylus', 
-                'handlebars',
-                'cname');
-});
+gulp.task('build', () => {
+  runSequence(
+    'clean:dist',
+    'copy:assets',
+    'stylus', 
+    'handlebars',
+    'cname',
+  )
+})
 
-gulp.task('server', function() {
-    runSequence('clean:dist',
-                'copy:assets',
-                'stylus', 
-                'handlebars',
-                'cname',
-                'connect',
-                'watch');
-});
+gulp.task('server', () => {
+  runSequence(
+    'clean:dist',
+    'copy:assets',
+    'stylus', 
+    'handlebars',
+    'cname',
+    'connect',
+    'watch',
+  )
+})
